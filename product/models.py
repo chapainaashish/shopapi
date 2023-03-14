069c36410ede7569b7ee278e2c33dd4a45da46ae
Aashish Chapain <ashishchapain86@gmail.com>
2023-03-14 17:32:49 +0545

    feat: complete model

--- product/models.py
+++ product/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 
+from customer.models import Customer
+
 
 class Category(models.Model):
     name = models.CharField(max_length=255)
@@ -12,3 +14,9 @@ class Product(models.Model):
     description = models.TextField()
     price = models.DecimalField(max_digits=8, decimal_places=2)
     category = models.ForeignKey(Category, on_delete=models.PROTECT)
+
+
+class Review(models.Model):
+    customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    review = models.TextField()
